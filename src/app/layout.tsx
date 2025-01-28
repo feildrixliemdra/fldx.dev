6194a69d9348ac7401e74b7effc0d91e13299535
feildrixliemdra <feildrix.liemdra@efishery.com>
2025-01-28 13:33:25 +0700

    feat: add vercel analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import "@/once-ui/styles/index.scss";
-import "@/once-ui/tokens/index.scss";
+import '@/once-ui/styles/index.scss'
+import '@/once-ui/tokens/index.scss'
 
-import classNames from "classnames";
+import classNames from 'classnames'
 
-import { Footer, Header, RouteGuard } from "@/components";
-import { baseURL, effects, style } from "@/app/resources";
+import { Footer, Header, RouteGuard } from '@/components'
+import { baseURL, effects, style } from '@/app/resources'
 
-import { Inter } from "next/font/google";
-import { Source_Code_Pro } from "next/font/google";
+import { Inter } from 'next/font/google'
+import { Source_Code_Pro } from 'next/font/google'
 
-import { person, home } from "@/app/resources/content";
-import { Background, Column, Flex, ToastProvider } from "@/once-ui/components";
+import { person, home } from '@/app/resources/content'
+import { Background, Column, Flex, ToastProvider } from '@/once-ui/components'
+import { Analytics } from '@vercel/analytics/react'
 
 export async function generateMetadata() {
   return {
@@ -19,11 +20,11 @@ export async function generateMetadata() {
     description: home.description,
     openGraph: {
       title: `${person.firstName}'s Portfolio`,
-      description: "Portfolio website showcasing my work.",
+      description: 'Portfolio website showcasing my work.',
       url: baseURL,
       siteName: `${person.firstName}'s Portfolio`,
-      locale: "en_US",
-      type: "website",
+      locale: 'en_US',
+      type: 'website',
     },
     robots: {
       index: true,
@@ -31,49 +32,49 @@ export async function generateMetadata() {
       googleBot: {
         index: true,
         follow: true,
-        "max-video-preview": -1,
-        "max-image-preview": "large",
-        "max-snippet": -1,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
       },
     },
-  };
+  }
 }
 
 const primary = Inter({
-  variable: "--font-primary",
-  subsets: ["latin"],
-  display: "swap",
-});
+  variable: '--font-primary',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 type FontConfig = {
-  variable: string;
-};
+  variable: string
+}
 
 /*
 	Replace with code for secondary and tertiary fonts
 	from https://once-ui.com/customize
 */
-const secondary: FontConfig | undefined = undefined;
-const tertiary: FontConfig | undefined = undefined;
+const secondary: FontConfig | undefined = undefined
+const tertiary: FontConfig | undefined = undefined
 /*
  */
 
 const code = Source_Code_Pro({
-  variable: "--font-code",
-  subsets: ["latin"],
-  display: "swap",
-});
+  variable: '--font-code',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <Flex
-      as="html"
-      lang="en"
-      background="page"
+      as='html'
+      lang='en'
+      background='page'
       data-neutral={style.neutral}
       data-brand={style.brand}
       data-accent={style.accent}
@@ -85,13 +86,19 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       data-transition={style.transition}
       className={classNames(
         primary.variable,
-        secondary ? secondary.variable : "",
-        tertiary ? tertiary.variable : "",
-        code.variable,
+        secondary ? secondary.variable : '',
+        tertiary ? tertiary.variable : '',
+        code.variable
       )}
     >
       <ToastProvider>
-        <Column style={{ minHeight: "100vh" }} as="body" fillWidth margin="0" padding="0">
+        <Column
+          style={{ minHeight: '100vh' }}
+          as='body'
+          fillWidth
+          margin='0'
+          padding='0'
+        >
           <Background
             mask={{
               cursor: effects.mask.cursor,
@@ -139,24 +146,25 @@ export default async function RootLayout({ children }: RootLayoutProps) {
               opacity: effects.lines.opacity as any,
             }}
           />
-          <Flex fillWidth minHeight="16"></Flex>
+          <Flex fillWidth minHeight='16'></Flex>
           <Header />
           <Flex
-            position="relative"
+            position='relative'
             zIndex={0}
             fillWidth
-            paddingY="l"
-            paddingX="l"
-            horizontal="center"
+            paddingY='l'
+            paddingX='l'
+            horizontal='center'
             flex={1}
           >
-            <Flex horizontal="center" fillWidth minHeight="0">
+            <Flex horizontal='center' fillWidth minHeight='0'>
               <RouteGuard>{children}</RouteGuard>
+              <Analytics />
             </Flex>
           </Flex>
           <Footer />
         </Column>
       </ToastProvider>
     </Flex>
-  );
+  )
 }
